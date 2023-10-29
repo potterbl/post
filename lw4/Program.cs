a413a661fd1a1294a2fee2a5127974f93314506a
potterbl <zeexample42@gmail.com>
2023-10-29 20:13:00 +0200

    init

---
+++ lw4/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Базовий клас Відправлення (абстрактний)
+public abstract class Shipment
+{
+    public int Id { get; set; }
+    public string Sender { get; set; }
+    public string Receiver { get; set; }
+    public double Weight { get; set; }
+    public double Cost { get; set; }
+
+    public Shipment(int id, string sender, string receiver, double weight)
+    {
+        Id = id;
+        Sender = sender;
+        Receiver = receiver;
+        Weight = weight;
+    }
+
+    public abstract void CalculateCost();
+}
+
+// Клас Лист, успадкований від Відправлення
+public class Letter : Shipment
+{
+    public string Content { get; set; }
+
+    public Letter(int id, string sender, string receiver, double weight, string content)
+        : base(id, sender, receiver, weight)
+    {
+        Content = content;
+    }
+
+    public override void CalculateCost()
+    {
+        // Розрахунок вартості відправлення для листа
+        // Наприклад, базуючись на вазі і типі доставки
+        Cost = Weight * 0.2;
+    }
+}
+
+// Клас Посилка, успадкований від Відправлення
+public class Package : Shipment
+{
+    public bool IsFragile { get; set; }
+
+    public Package(int id, string sender, string receiver, double weight, bool isFragile)
+        : base(id, sender, receiver, weight)
+    {
+        IsFragile = isFragile;
+    }
+
+    public override void CalculateCost()
+    {
+        // Розрахунок вартості відправлення для посилки
+        // Наприклад, базуючись на вазі, хрупкості та типі доставки
+        Cost = Weight * 0.5 + (IsFragile ? 10 : 0);
+    }
+}
+
+// Інтерфейс для взаємодії з Поштовим відділенням
+public interface IPostOffice
+{
+    void SendShipment(Shipment shipment);
+    Shipment ReceiveShipment(int id);
+}
+
+// Клас Поштове відділення
+public class PostOffice : IPostOffice
+{
+    private List<Shipment> shipments = new List<Shipment>();
+
+    public void SendShipment(Shipment shipment)
+    {
+        shipment.CalculateCost(); // Розрахунок вартості
+        shipments.Add(shipment);
+    }
+
+    public Shipment ReceiveShipment(int id)
+    {
+        Shipment shipment = shipments.Find(s => s.Id == id);
+        if (shipment != null)
+        {
+            shipments.Remove(shipment);
+        }
+        return shipment;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PostOffice postOffice = new PostOffice();
+
+        while (true)
+        {
+            Console.WriteLine("Поштове відділення");
+            Console.WriteLine("1. Відправити лист");
+            Console.WriteLine("2. Відправити посилку");
+            Console.WriteLine("3. Отримати відправлення");
+            Console.WriteLine("4. Вихід");
+
+            Console.Write("Оберіть опцію: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    SendLetter(postOffice);
+                    break;
+                case 2:
+                    SendPackage(postOffice);
+                    break;
+                case 3:
+                    ReceiveShipment(postOffice);
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+                    break;
+            }
+        }
+    }
+
+    static void SendLetter(PostOffice postOffice)
+    {
+        Console.Write("Введіть ID: ");
+        int id = int.Parse(Console.ReadLine());
+        Console.Write("Введіть відправника: ");
+        string sender = Console.ReadLine();
+        Console.Write("Введіть отримувача: ");
+        string receiver = Console.ReadLine();
+        Console.Write("Введіть вагу (грами): ");
+        double weight = double.Parse(Console.ReadLine());
+        Console.Write("Введіть зміст листа: ");
+        string content = Console.ReadLine();
+
+        Letter letter = new Letter(id, sender, receiver, weight, content);
+        postOffice.SendShipment(letter);
+        Console.WriteLine("Лист відправлено!");
+    }
+
+    static void SendPackage(PostOffice postOffice)
+    {
+        Console.Write("Введіть ID: ");
+        int id = int.Parse(Console.ReadLine());
+        Console.Write("Введіть відправника: ");
+        string sender = Console.ReadLine();
+        Console.Write("Введіть отримувача: ");
+        string receiver = Console.ReadLine();
+        Console.Write("Введіть вагу (грами): ");
+        double weight = double.Parse(Console.ReadLine());
+        Console.Write("Чи є посилка хрупкою? (Так/Ні): ");
+        bool isFragile = Console.ReadLine().Equals("Так", StringComparison.OrdinalIgnoreCase);
+
+        Package package = new Package(id, sender, receiver, weight, isFragile);
+        postOffice.SendShipment(package);
+        Console.WriteLine("Посилка відправлена!");
+    }
+
+    static void ReceiveShipment(PostOffice postOffice)
+    {
+        Console.Write("Введіть ID відправлення, яке ви бажаєте отримати: ");
+        int id = int.Parse(Console.ReadLine());
+
+        Shipment shipment = postOffice.ReceiveShipment(id);
+        if (shipment != null)
+        {
+            Console.WriteLine("Відправлення отримано:");
+            Console.WriteLine($"ID: {shipment.Id}");
+            Console.WriteLine($"Відправник: {shipment.Sender}");
+            Console.WriteLine($"Отримувач: {shipment.Receiver}");
+            Console.WriteLine($"Вага: {shipment.Weight} г");
+            Console.WriteLine($"Вартість: {shipment.Cost} грн");
+        }
+        else
+        {
+            Console.WriteLine("Відправлення з таким ID не знайдено.");
+        }
+    }
+}
---
+++ lw4/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("lw4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lw4")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97F70E99-D5C2-44B5-B2E8-1209F90EC698")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
